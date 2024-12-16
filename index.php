176d935c0f2ef2719e38f9a91b5629d12760f125
rambyai <jason@ramby.ai>
2024-12-15 23:00:28 -0800

    Update title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Design with AI</title>
+    <title>Penpot Rapid Prototyping with AI</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="index.css">

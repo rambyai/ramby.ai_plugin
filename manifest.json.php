d4f5059037bcc025081eae3238dc0de3f17054ae
jasondeegan <jld0417@gmail.com>
2024-12-15 22:39:52 -0800

    adding manifest to .gitignore

--- manifest.json.php
+++ manifest.json.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json; charset=utf-8");
 
 echo json_encode([
   "name" => "Design with AI",
-  "host" => "http://localhost",
+  "host" => "http://ramby.ai/plugin/",
   "description" => "Use AI to help create from scratch or alter existing designs.",
   "author" => "Jason Deegan",
   "code" => "plugin.js",
